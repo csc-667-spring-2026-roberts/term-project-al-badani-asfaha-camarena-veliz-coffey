b4801cef8e27d599bcb059e982f821ff74e1dcb6
Ghadeersfsu <143347379+Ghadeersfsu@users.noreply.github.com>
2026-03-02 14:03:06 -0800

    fix: linting errors (#2)
    - replaced unused parameters to "_"
    - changed import file extension to js

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -2,12 +2,12 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get("/", (request, response) => {
-    response.send(`<h1>Hello World!</h1><p>${new Date().toLocaleDateString()}</p>`);
+router.get("/", (_, response) => {
+  response.send(`<h1>Hello World!</h1><p>${new Date().toLocaleDateString()}</p>`);
 });
 
 router.get("/:id", (request, response) => {
-    response.send(`Hi at ${request.params.id}`);
+  response.send(`Hi at ${request.params.id}`);
 });
 
 export default router;
