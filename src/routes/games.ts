1f093331fbb5b111425697acc345a903525e3d18
ronan coffey <rcoffey@sfsu.edu>
2026-05-14 14:23:16 -0700

    fixed bug referencing non existent user

--- src/routes/games.ts
+++ src/routes/games.ts
@@ -5,26 +5,11 @@ import { requireAuth } from "../middleware/auth.js";
 
 const router = Router();
 
-// router.get("/", async (_request, response) => {
-//   const games = await Games.list();
-
-//   response.json(games);
-// });
-
-// router.post("/", requireAuth, async (request, response) => {
-//   const user = request.session.user as { id: number };
-//   const game = await Games.create(user.id);
-//   SSE.broadcast({ type: "game_updates", data: await Games.list() });
-
-//   response.json(game);
-// });
-
 router.get("/:gameId", requireAuth, (request, response) => {
   const gameId = request.params.gameId;
   const userId = request.session.user?.id;
-  // if gameId
-  // Games.ge
-  response.status(200).render("game", { gameId: gameId, user: userId });
+
+  // response.status(200).render("game", { gameId: gameId, user: userId });
 
   if (!userId) {
     response.redirect("/login");
